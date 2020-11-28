1c76e724e74eadae2bc542419ff6d68a9124453d
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2020-11-28 19:59:38 +0530

    Finished upto IIFE

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,191 +1,191 @@
-// console.log("Hello World!");
+console.log("Hello World!");
 
-// let obj = { name: "kmaran", age: 23 };
-// console.table(obj);
-// console.error("This is an error");
-// console.info("This is an info");
-// console.warn("This is a warning");
+let obj = { name: "kmaran", age: 23 };
+console.table(obj);
+console.error("This is an error");
+console.info("This is an info");
+console.warn("This is a warning");
 
-// console.time("myTimer");
-// console.log("Taks Running!");
-// console.log("Taks Running!");
-// console.log("Taks Running!");
-// console.log("Taks Running!");
-// console.log("Taks Running!");
-// console.timeEnd("myTimer");
+console.time("myTimer");
+console.log("Taks Running!");
+console.log("Taks Running!");
+console.log("Taks Running!");
+console.log("Taks Running!");
+console.log("Taks Running!");
+console.timeEnd("myTimer");
 
-// const obj = {
-//   name: "kmaran",
-//   age: 23,
-// };
-// obj.name = "karan";
-// console.log(obj);
+const obj = {
+  name: "kmaran",
+  age: 23,
+};
+obj.name = "karan";
+console.log(obj);
 
-// const myArr = [1, 2, 3, 4];
-// console.log(myArr);
-// myArr.push(5);
-// console.log(myArr);
-// myArr[4] = "Added";
-// console.log(myArr);
+const myArr = [1, 2, 3, 4];
+console.log(myArr);
+myArr.push(5);
+console.log(myArr);
+myArr[4] = "Added";
+console.log(myArr);
 
-// let name = "Maran Sowthri Kalailingam";
-// let age = 21;
-// let salary;
-// let secrets = null;
-// console.log(typeof secrets);
-// let obj = { name, age };
-// console.log(typeof obj);
-// let symbol = Symbol();
-// console.log(typeof symbol);
+let name = "Maran Sowthri Kalailingam";
+let age = 21;
+let salary;
+let secrets = null;
+console.log(typeof secrets);
+let obj = { name, age };
+console.log(typeof obj);
+let symbol = Symbol();
+console.log(typeof symbol);
 
-// let val = 5;
-// console.log(val);
-// console.log(typeof val);
+let val = 5;
+console.log(val);
+console.log(typeof val);
 
-// val = String(val);
-// console.log(val);
-// console.log(typeof val);
+val = String(val);
+console.log(val);
+console.log(typeof val);
 
-// let val = [1, 2, 3, 4];
-// console.log(val);
-// console.log(String(val));
-// console.log(val.toString());
+let val = [1, 2, 3, 4];
+console.log(val);
+console.log(String(val));
+console.log(val.toString());
 
-// let val = "53.127";
-// console.log(val);
-// val = Number(val);
-// console.log(val.toFixed(2));
+let val = "53.127";
+console.log(val);
+val = Number(val);
+console.log(val.toFixed(2));
 
-// let val = 54.65;
-// // val = parseInt(val);
+let val = 54.65;
 // val = parseInt(val);
-// console.log(val);
-// console.log(typeof val);
+val = parseInt(val);
+console.log(val);
+console.log(typeof val);
 
-// let val1 = 200;
-// let val2 = 10;
-// let sum = val1 + val2;
-// sum = val1 / val2;
-// console.log(typeof sum, sum);
+let val1 = 200;
+let val2 = 10;
+let sum = val1 + val2;
+sum = val1 / val2;
+console.log(typeof sum, sum);
 
-// console.log(Math.PI);
-// console.log(Math.E);
-// console.log(1 / 3);
-// console.log(Math.abs(1 - 3));
-// let ans = 3 / 2;
-// console.log(typeof ans.toFixed(2));
+console.log(Math.PI);
+console.log(Math.E);
+console.log(1 / 3);
+console.log(Math.abs(1 - 3));
+let ans = 3 / 2;
+console.log(typeof ans.toFixed(2));
 
-// let randomNumber = Math.random();
-// console.log(randomNumber);
+let randomNumber = Math.random();
+console.log(randomNumber);
 
-// let randomNum1to100 = Math.floor(Math.random() * 100 + 1);
-// console.log(randomNum1to100);
+let randomNum1to100 = Math.floor(Math.random() * 100 + 1);
+console.log(randomNum1to100);
 
-// let sentence = 'That"s awesome I can"t wait';
+let sentence = 'That"s awesome I can"t wait';
+console.log(sentence);
+
+let firstName = "Maran";
+let lastName = "Sowthri";
+console.log(firstName.concat(" ", lastName, " Kalailingam"));
+
+let name = "kmaran";
+console.log(name[0]);
+console.log(name.indexOf("k"));
+console.log(name.lastIndexOf("a"));
+name = name.substring(0, name.length);
+name = name.slice(-3, 5);
+
+let sentence = "Hello there my name is Maran";
+let words = sentence.split(" ");
+let slug = words.join("-").toLowerCase();
+console.log(slug);
+
+let template = "My name is XXXX, XXXX";
+let sentence = template.replaceAll("XXXX", "kmaran");
+let included = sentence.includes("kmaran");
 // console.log(sentence);
+console.log(included);
 
-// let firstName = "Maran";
-// let lastName = "Sowthri";
-// console.log(firstName.concat(" ", lastName, " Kalailingam"));
+let myArr = [1, 2, 3, 4, 5];
+myArr = Array(3, 2, 4, 4);
+console.log(myArr);
+console.log(Array.isArray(myArr));
+console.log(myArr.push(3));
+console.log(myArr.unshift(0));
+console.log(myArr.pop());
+console.log(myArr.splice(1, 2, -2, -3));
+console.log(myArr.reverse());
 
-// let name = "kmaran";
-// console.log(name[0]);
-// console.log(name.indexOf("k"));
-// console.log(name.lastIndexOf("a"));
-// name = name.substring(0, name.length);
-// name = name.slice(-3, 5);
+let myArr1 = [1, 2, 3, 4];
+let myArr2 = [5, 6, 7, 8];
+let newArr = myArr1.concat(myArr2);
+console.log(newArr);
 
-// let sentence = "Hello there my name is Maran";
-// let words = sentence.split(" ");
-// let slug = words.join("-").toLowerCase();
-// console.log(slug);
+let myArr1 = [1, 2, 7, 3, 4, 10];
+console.log(myArr1.sort((x, y) => x - y));
+console.log(myArr1);
 
-// let template = "My name is XXXX, XXXX";
-// let sentence = template.replaceAll("XXXX", "kmaran");
-// let included = sentence.includes("kmaran");
-// // console.log(sentence);
-// console.log(included);
+let myArr1 = [8, 1, 2, 7, 3, 4, 10];
+console.log(myArr1.find((x) => x < 50));
 
-// let myArr = [1, 2, 3, 4, 5];
-// myArr = Array(3, 2, 4, 4);
-// console.log(myArr);
-// console.log(Array.isArray(myArr));
-// console.log(myArr.push(3));
-// console.log(myArr.unshift(0));
-// console.log(myArr.pop());
-// console.log(myArr.splice(1, 2, -2, -3));
-// console.log(myArr.reverse());
+date = new Date("11/29/2020");
+console.log(date);
 
-// let myArr1 = [1, 2, 3, 4];
-// let myArr2 = [5, 6, 7, 8];
-// let newArr = myArr1.concat(myArr2);
-// console.log(newArr);
+date = new Date("11-29-2020");
+console.log(date);
 
-// let myArr1 = [1, 2, 7, 3, 4, 10];
-// console.log(myArr1.sort((x, y) => x - y));
-// console.log(myArr1);
+date = new Date("11/29/2020 11:23:25");
+console.log(date);
 
-// let myArr1 = [8, 1, 2, 7, 3, 4, 10];
-// console.log(myArr1.find((x) => x < 50));
+date = new Date("November 29 2020");
+console.log(date);
 
-// date = new Date("11/29/2020");
-// console.log(date);
+date = new Date();
+console.log(date.getDay());
+console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.setMonth(3));
+console.log(date);
 
-// date = new Date("11-29-2020");
-// console.log(date);
+const id = "123";
+if (id !== 123) {
+  console.log("CORRECT");
+} else {
+  console.log("INCORRECT");
+}
 
-// date = new Date("11/29/2020 11:23:25");
-// console.log(date);
+let id = 1;
+console.log(typeof dummy);
 
-// date = new Date("November 29 2020");
-// console.log(date);
+if (typeof id !== "undefined") {
+  console.log(`The ID is ${id}`);
+} else {
+  console.log("ID is not defined");
+}
 
-// date = new Date();
-// console.log(date.getDay());
-// console.log(date.getDate());
-// console.log(date.getMonth());
-// console.log(date.getFullYear());
-// console.log(date.setMonth(3));
-// console.log(date);
+let age = 8;
+let type;
 
-// const id = "123";
-// if (id !== 123) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
+age > 21 ? (type = "Major") : age > 10 ? (type = "Minor") : (type = "child");
+console.log(type);
 
-// let id = 1;
-// console.log(typeof dummy);
+const type = "child";
 
-// if (typeof id !== "undefined") {
-//   console.log(`The ID is ${id}`);
-// } else {
-//   console.log("ID is not defined");
-// }
+switch (type) {
+  case "major": {
+    console.log("He is a Major");
+    break;
+  }
+  case "minor": {
+    console.log("He is a Minor");
+    break;
+  }
+  default: {
+    console.log("Category not found");
+  }
+}
 
-// let age = 8;
-// let type;
-
-// age > 21 ? (type = "Major") : age > 10 ? (type = "Minor") : (type = "child");
-// console.log(type);
-
-// const type = "child";
-
-// switch (type) {
-//   case "major": {
-//     console.log("He is a Major");
-//     break;
-//   }
-//   case "minor": {
-//     console.log("He is a Minor");
-//     break;
-//   }
-//   default: {
-//     console.log("Category not found");
-//   }
-// }
-
-// (() => {
-//   console.log("IIFE");
-// })();
+(() => {
+  console.log("IIFE");
+})();
