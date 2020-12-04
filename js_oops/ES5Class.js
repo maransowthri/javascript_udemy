fc527c71c1b27c76dcfcf2c12220fd6c47146b52
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2020-12-04 20:16:46 +0530

    Uncommented  ES5 classses

--- js_oops/ES5Class.js
+++ js_oops/ES5Class.js
@@ -1,77 +1,77 @@
 /* ES5 objects & constructor */
-// function Person(name, dob) {
-//   this.name = name;
-//   this.dob = new Date(dob);
-//   this.getAge = function () {
-//     curDate = new Date();
-//     return curDate.getFullYear() - this.dob.getFullYear();
-//   };
-// }
+function Person(name, dob) {
+  this.name = name;
+  this.dob = new Date(dob);
+  this.getAge = function () {
+    curDate = new Date();
+    return curDate.getFullYear() - this.dob.getFullYear();
+  };
+}
 
-// maran = new Person("kmaran", "10-18-1996");
-// console.log(maran);
-// console.log(maran.getAge());
+maran = new Person("kmaran", "10-18-1996");
+console.log(maran);
+console.log(maran.getAge());
 
 /* ES5 prototypes */
 
-// function Person(name, dob) {
-//   this.name = name;
-//   this.dob = new Date(dob);
-// }
+function Person(name, dob) {
+  this.name = name;
+  this.dob = new Date(dob);
+}
 
-// Person.prototype.getAge = function () {
-//   curDate = new Date();
-//   return curDate.getFullYear() - this.dob.getFullYear();
-// };
+Person.prototype.getAge = function () {
+  curDate = new Date();
+  return curDate.getFullYear() - this.dob.getFullYear();
+};
 
-// Person.prototype.changeName = function (newName) {
-//   this.name = newName;
-// };
+Person.prototype.changeName = function (newName) {
+  this.name = newName;
+};
 
-// maran = new Person("kmaran", "10-18-1996");
-// console.log(maran);
-// console.log(maran.getAge());
-// maran.changeName("karan");
-// console.log(maran.name);
-// console.log(maran.hasOwnProperty("name"));
-// console.log(maran.hasOwnProperty("changeName"));
+maran = new Person("kmaran", "10-18-1996");
+console.log(maran);
+console.log(maran.getAge());
+maran.changeName("karan");
+console.log(maran.name);
+console.log(maran.hasOwnProperty("name"));
+console.log(maran.hasOwnProperty("changeName"));
 
 /* ES5 prototypal inheritance */
 
-// function Person(name, dob) {
-//   this.name = name;
-//   this.dob = new Date(dob);
-// }
+function Person(name, dob) {
+  this.name = name;
+  this.dob = new Date(dob);
+}
 
-// Person.prototype.getAge = function () {
-//   curDate = new Date();
-//   return curDate.getFullYear() - this.dob.getFullYear();
-// };
+Person.prototype.getAge = function () {
+  curDate = new Date();
+  return curDate.getFullYear() - this.dob.getFullYear();
+};
 
-// function Student(name, dob, std) {
-//   Person.call(this, name, dob);
-//   this.std = std;
-// }
+function Student(name, dob, std) {
+  Person.call(this, name, dob);
+  this.std = std;
+}
 
-// Student.prototype = Object.create(Person.prototype);
-// Student.prototype.constructor = Student;
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
 
-// const student1 = new Student("kmaran", "12/04/2020", "IX");
+const student1 = new Student("kmaran", "12/04/2020", "IX");
 
-// console.log(student1);
-// console.log(student1.getAge());
+console.log(student1);
+console.log(student1.getAge());
 
 /* Object create prototypes */
 
-// const protoTyeps = {
-//   greeting: function () {
-//     return `Hello ${this.name}!`;
-//   },
-// };
+const protoTyeps = {
+  greeting: function () {
+    return `Hello ${this.name}!`;
+  },
+};
 
-// person1 = Object.create(protoTyeps);
-// person1.name = "kmaran";
-// console.log(person1.greeting());
+person1 = Object.create(protoTyeps);
+person1.name = "kmaran";
+console.log(person1.greeting());
 
-// person1 = Object.create(protoTyeps, { name: { value: "kmaran" } });
-// console.log(person1.greeting());
+person1 = Object.create(protoTyeps, { name: { value: "kmaran" } });
+console.log(person1.greeting());
